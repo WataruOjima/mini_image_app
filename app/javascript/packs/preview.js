9b21037377477b355a46187e0778ac29452e1e40
WataruOjima <wataru.ojima20@gmail.com>
2021-02-06 19:59:31 +0900

    2枚目以降の投稿に必要な要素を生成
    必要な要素をJavaScript側で生成
    input要素を追加
    document.createElement('input')と記述することで、input要素をJavaScript側で生成できます。

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,20 +1,24 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+  document.addEventListener('DOMContentLoaded', function() {
+    const ImageList = document.getElementById('image-list')
 
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
-      const imageElement = document.createElement('div');
+      const imageElement = document.createElement('div')
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
 
       // 表示する画像を生成
-      const blobImage = document.createElement('img');
+      const blobImage = document.createElement('img')
       blobImage.setAttribute('src', blob);
 
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
-    };
-    document.getElementById('message_image').addEventListener('change', function(e){
+    }
+
+    document.getElementById('message_image').addEventListener('change', (e) => {
       // 画像が表示されている場合のみ、すでに存在している画像を削除する
       // ここから削除
       // const imageContent = document.querySelector('img');
@@ -26,7 +30,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
-      createImageHTML(blob);
+      createImageHTML(blob)
     });
   });
 }
